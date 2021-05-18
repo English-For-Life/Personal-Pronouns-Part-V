11b9ea474246acd72c31e7c78e6232df994dc63a
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-05-18 12:52:03 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,54 @@
+//Array of objects
+const quiz = [
+    {
+        q:"I don't know Mike's phone number. Could you write ___ for ___ ?",
+        options:["he/me", "it / me", "it / I"],
+        answer: 1
+    },
+    {
+        q:"Don't help me with this exercise! ___ can do ___ by myself.",
+        options:["I / it", "I / him", "We / them"],
+        answer: 0   
+    },
+    {
+        q:"My computer isn't working. ___ is crashed again. Can you have a look at ___ .",
+        options:["It / him", "He / him", "It / it"],
+        answer: 2
+    },
+    {
+        q:"What are your kids doing? - ___ are playing outside. ",
+        options:[ "He", "Them", "They"],
+        answer: 2
+    },
+    {
+        q:"We are very worried about you. Tell ___ what is happening.",
+        options:["us", "we", "her"],
+        answer: 0
+    },
+    {
+        q:"Alex loves Rachel. He offered ___ to marry ___ .",
+        options:["she / he", "her / him", "her / he"],
+        answer: 1
+    },
+    {
+        q:"My husband and I bought a car. ___ like ___ very much.",
+        options:["We / it", "We / her", "We / she"],
+        answer: 0
+    },
+    {
+        q:"Do you have a new skirt? Show ___ to me.",
+        options:["her", "it", "them"],
+        answer: 1
+    },
+    {
+        q:"These knives are very sharp. Be careful with ___ .",
+        options:["it", "them", "he"],
+        answer: 1
+    },
+    {
+        q:"He didn't like the way she spoke to ___ .",
+        options:["them", "he", "him"],
+        answer: 2
+    }
+
+]
